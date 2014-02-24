a598134b3048901b9601f46d028a5c477f2cb52a
adrianObel <obeladrian@gmail.com>
2014-02-24 15:28:11 -0430

    Initial support for dnode

--- supervisor.js
+++ supervisor.js
@@ -1,15 +1,36 @@
+// Set up dependencies
 var Agenda = require('agenda');
-var dnonde = require('dnode');
+var dnode = require('dnode');
 
+// Agenda Config
 var agenda = new Agenda()
   .database(process.env.DATABASE_URL, process.env.DATABASE_COLLECTION)
   .processEvery('10 seconds');
 
+var workers = {};
 
-agenda.define('test done', function scheduler(job) {
-  console.log('done!');
-});
 
-agenda.every('10 seconds', 'test done');
+// Set up RPC server
+var server = dnode({
+  initWorker: function(worker, callback) {
+    
+    console.log('Init worker' + worker);
 
-agenda.start();
+    // Add a worker 
+    workers[worker] = true;
+    agenda.start();
+    callback('SERVER listening');
+  },
+  getJobs: function(worker, callback) {
+
+    agenda.define('find jobs', scheduler);
+
+    function scheduler(job) {
+      callback(worker);
+    }
+
+    agenda.every('20 seconds', 'find jobs');
+  }
+})
+
+server.listen(process.env.PORT);
