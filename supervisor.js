19770ed9a51ae847243735bbd9ffa4c6a6fef24a
adrianObel <obeladrian@gmail.com>
2014-02-24 18:54:28 -0430

    Get url and selectors from env

--- supervisor.js
+++ supervisor.js
@@ -1,35 +1,52 @@
 // Set up dependencies
 var Agenda = require('agenda');
 var dnode = require('dnode');
+var format = require('util').format;
+
+// Set uo environment variables
+var PROTOCOL = process.env.PROTOCOL;
+var baseUrl = process.env.BASE_URL; 
+var section = process.env.SECTION;
+var jobSelector = process.env.JOB_SELECTOR;
+var nextPageSelector = process.env.NEXT_PAGE;
 
 // Agenda Config
 var agenda = new Agenda()
   .database(process.env.DATABASE_URL, process.env.DATABASE_COLLECTION)
-  .processEvery('10 seconds');
+  .processEvery('5 seconds');
+
 
 var workers = {};
-
+var site = {
+  url: format('%s://%s', PROTOCOL, baseUrl),
+  section: section,
+  selectors: {
+    job: jobSelector,
+    nextPage: nextPageSelector
+  }
+};
 
 // Set up RPC server
 var server = dnode({
   initWorker: function(worker, callback) {
     
-    console.log('Init worker' + worker);
+    console.log('Init worker' + worker.id);
 
     // Add a worker 
-    workers[worker] = true;
+    workers[worker.id] = true;
+
     agenda.start();
     callback('SERVER listening');
   },
   getJobs: function(worker, callback) {
 
-    agenda.define('find jobs', scheduler);
+    agenda.define('find jobs', getJobs);
 
-    function scheduler(job) {
-      callback(worker);
+    function getJobs(job) {
+      callback(site);
     }
 
-    agenda.every('20 seconds', 'find jobs');
+    agenda.every('5 seconds', 'find jobs');
   }
 })
 
