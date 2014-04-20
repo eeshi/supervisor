5bfc6aa12cb56555b0ffbc0509895ecbaa1c44e8
Adrian Obelmejias <obeladrian@gmail.com>
2014-04-20 13:59:29 -0430

    Set up Agenda and RPC server

--- supervisor.js
+++ supervisor.js
@@ -1,56 +1,13 @@
-// Set up dependencies
 var Agenda = require('agenda');
 var dnode = require('dnode');
-var format = require('util').format;
 
-// Set uo environment variables
-var PROTOCOL = process.env.PROTOCOL;
-var baseUrl = process.env.BASE_URL; 
-var section = process.env.SECTION;
-var jobSelector = process.env.JOB_SELECTOR;
-var nextPageSelector = process.env.NEXT_PAGE;
+var DATABASE_URL = process.env['DATABASE_URL'];
 
-// Agenda Config
 var agenda = new Agenda()
-  .database(process.env.DATABASE_URL, process.env.DATABASE_COLLECTION)
+  .database(DATABASE_URL, '/agenda')
   .processEvery('30 seconds');
 
-
-var workers = {};
-var site = {
-  url: format('%s://%s', PROTOCOL, baseUrl),
-  section: section,
-  selectors: {
-    job: jobSelector,
-    nextPage: nextPageSelector
-  }
-};
-
-// Set up RPC server
-var server = dnode({
-  initWorker: function(worker, callback) {
-    
-    console.log('Init worker: ' + worker.id);
-
-    // Add a worker 
-    workers[worker.id] = true;
-
-    agenda.start();
-    callback({response: 'SERVER listening'});
-  },
-  getJobs: function(worker, callback) {
-    agenda.define('find jobs', function getJobs(job) {
-      callback(site);
-    });
-    agenda.every('24 hours', 'find jobs');
-    
-  },
-  saveJobs: function(worker, callback) {
-    console.log(worker.jobsCollected);
-    callback({response: jobs.length + ' jobs saved'});
-  }
-});
+var d = dnode.connect(PORT);
 
 
 
-server.listen(process.env.PORT);
