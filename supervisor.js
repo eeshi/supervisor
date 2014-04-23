529f54fbc17bb35a0c6d9342de78d89e7df4ca32
Adrian Obelmejias <obeladrian@gmail.com>
2014-04-23 17:58:19 -0430

    Pass url to Worker object as argument and let it internally set up dnode connection instead of explicitly

--- supervisor.js
+++ supervisor.js
@@ -1,5 +1,4 @@
 var Agenda = require('agenda');
-var dnode = require('dnode');
 var utils = require('./utils');
 
 var WorkerCollection = require('./lib/WorkerCollection');
@@ -14,21 +13,12 @@ var WORKER_URLS = process.env['WORKERS'].split(',');
 
 var workers = new WorkerCollection();
 
-WORKER_URLS.map(function(url) {
-  
-  try {
+WORKER_URLS.forEach(function(url) {
 
-    var seg = url.split(':');
-    var conn = dnode.connect(seg[0], seg[1]);
-    var worker = new Worker(conn);
-
-    return workers.addWorker(worker);
-
-  } catch(err) { console.log(err); }
+  var worker = new Worker(url);
+  workers.addWorker(worker);
 
 });
-
-workers.init();
 
 // agenda.start();
 
