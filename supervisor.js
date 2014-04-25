563ab72ac92646b5e9a88910eaaeb9ce3f6a4e58
Adrian Obelmejias <obeladrian@gmail.com>
2014-04-25 11:26:28 -0430

    Started Agenda

--- supervisor.js
+++ supervisor.js
@@ -1,17 +1,18 @@
 var Agenda = require('agenda');
 var utils = require('./utils');
 
+var api = require('./lib/api');
 var WorkerCollection = require('./lib/WorkerCollection');
 var Worker = require('./lib/Worker');
 
 var DATABASE_URL = process.env['DATABASE_URL'];
 var WORKER_URLS = process.env['WORKERS'].split(',');
 
-// var agenda = new Agenda()
-//   .database(DATABASE_URL, '/scheduler')
-//   .processEvery('30 seconds');
+var agenda = new Agenda()
+  .database(DATABASE_URL, '/scheduler')
+  .processEvery('30 seconds');
 
-var workerCollection = new WorkerCollection({ refresh: 5000 });
+var workerCollection = new WorkerCollection({ refresh: 30 * 1000 }); // 1 Minute
 
 WORKER_URLS.forEach(function(url) {
 
@@ -20,4 +21,4 @@ WORKER_URLS.forEach(function(url) {
 
 });
 
-// agenda.start();
+agenda.start();
