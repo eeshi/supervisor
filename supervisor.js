cfc048cabe89ffe34fb55021b48419e828a90d7f
Adrian Obelmejias <obeladrian@gmail.com>
2014-05-26 22:54:07 -0430

    Removed classes that were used for reconnecting DNode

--- supervisor.js
+++ supervisor.js
@@ -5,8 +5,6 @@ var axon = require('axon');
 var req = axon.socket('req');
 
 var api = require('./lib/api');
-var WorkerCollection = require('./lib/WorkerCollection');
-var Worker = require('./lib/Worker');
 
 var DATABASE_URL = process.env['DATABASE_URL'];
 var WORKER_URLS = process.env['WORKERS'].split(',');
@@ -17,15 +15,6 @@ var WORKER_URLS = process.env['WORKERS'].split(',');
 
 
 var supervisor = new rpc.Client(req);
-
-// var workerCollection = new WorkerCollection({ refresh: 30 * 1000 }); // 1 Minute
-
-// WORKER_URLS.forEach(function(url) {
-
-//   var worker = new Worker(url);
-//   workerCollection.addWorker(worker);
-
-// });
 
 supervisor.call('wub', 'sent from the supervisor', function(err, message) {
   
