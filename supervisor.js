bfdc099a9302f761c5192b016d188984eea1bd82
Adrian Obelmejias <obeladrian@gmail.com>
2014-05-26 22:53:03 -0430

    Switched dnode for axon-rpc to facilitate reconnection to workers

--- supervisor.js
+++ supervisor.js
@@ -1,4 +1,8 @@
 var Agenda = require('agenda');
+var rpc = require('axon-rpc');
+var axon = require('axon');
+
+var req = axon.socket('req');
 
 var api = require('./lib/api');
 var WorkerCollection = require('./lib/WorkerCollection');
@@ -7,17 +11,29 @@ var Worker = require('./lib/Worker');
 var DATABASE_URL = process.env['DATABASE_URL'];
 var WORKER_URLS = process.env['WORKERS'].split(',');
 
-var agenda = new Agenda()
-  .database(DATABASE_URL, '/scheduler')
-  .processEvery('30 seconds');
+// var agenda = new Agenda()
+//   .database(DATABASE_URL, '/scheduler')
+//   .processEvery('30 seconds');
 
-var workerCollection = new WorkerCollection({ refresh: 30 * 1000 }); // 1 Minute
 
-WORKER_URLS.forEach(function(url) {
+var supervisor = new rpc.Client(req);
 
-  var worker = new Worker(url);
-  workerCollection.addWorker(worker);
+// var workerCollection = new WorkerCollection({ refresh: 30 * 1000 }); // 1 Minute
+
+// WORKER_URLS.forEach(function(url) {
+
+//   var worker = new Worker(url);
+//   workerCollection.addWorker(worker);
+
+// });
+
+supervisor.call('wub', 'sent from the supervisor', function(err, message) {
+  
+  if(err) throw err;
+
+  console.log(message);
 
 });
 
-agenda.start();
+req.connect(4000);
+// agenda.start();
